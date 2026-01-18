cc647152f77e7e626e03c3675489793f27042103
tconuorah <Terrenceonuorah@gmail.com>
2026-01-18 12:23:11 -0600

    update the environemnt files

--- infra/environments/staging/us-east-2/networking/backend.tf
+++ infra/environments/staging/us-east-2/networking/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket       = "terraform-gitops-file"
-    key          = "staging/us-east-2/networking/terraform.tfstate"
-    region       = "us-east-2"
+    bucket         = "terraform-gitops-file"
+    key            = "staging/us-east-2/networking/terraform.tfstate"
+    region         = "us-east-2"
     dynamodb_table = "terraform-lock"
-    encrypt      = true
+    encrypt        = true
   }
 }
